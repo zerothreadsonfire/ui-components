e9ce9b0c52717936676a8e86e43ceb95a3a10d9f
satyamKumar <satyamkumar2302@gmail.com>
2022-04-02 02:08:30 +0530

    navbar sticky functionality achieved

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,1 +1,20 @@
-console.log("hello")
+const navbar = document.querySelector(".navbar--full-width");
+let lastScroll=0;
+
+window.addEventListener("scroll", (e) => {
+  const currentScroll = window.scrollY;
+  
+  if(currentScroll <= 0) {
+    navbar.classList.remove("scroll-up");
+  }
+
+  if(currentScroll>lastScroll && !navbar.classList.contains("scroll-down")) {
+    navbar.classList.remove("scroll-up");
+    navbar.classList.add("scroll-down");
+  } else if(currentScroll<lastScroll && navbar.classList.contains("scroll-down")) {
+    navbar.classList.remove("scroll-down");
+    navbar.classList.add("scroll-up");
+  }
+
+  lastScroll = currentScroll;
+})
