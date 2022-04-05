18d1c14909e590c7e526a6c054b4cf94f7261786
satyamKumar <satyamkumar2302@gmail.com>
2022-04-05 13:37:14 +0530

    implement alt nav stly for intersection observer in navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,38 +1,50 @@
-// _smoothScroll();
-const section1 = document.querySelector(".section-1");
+class Navbar {
+  constructor() {
+    this.smoothScroll();
+    this.navbarAltStyle();
+  }
 
-const observer = new IntersectionObserver(function(entries, observer) {
-  entries.forEach(entry => {
+  navbarAltStyle = () => {
+    const sectionOne = document.querySelector(".cmp-banner");
+    const navbar = document.querySelector(".navbar--full-width");
 
-  })
-  console.log(entries);
-  console.log(observer);
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting==false) {
+          navbar.classList.add("navbar--alt-style");
+        } else {
+          navbar.classList.remove("navbar--alt-style");
+        }
+      });
+    }, {
+      rootMargin: "-100px 0px 0px 0px"
+    });
 
-}, {});
+    observer.observe(sectionOne);
+  }
 
-observer.observe(section1);
-
-
-function _smoothScroll() {
-  const navbar = document.querySelector(".navbar--full-width");
-  let lastScroll=0;
-  
-  window.addEventListener("scroll", (e) => {
-    const currentScroll = window.scrollY;
+  smoothScroll = () => {
+    const navbar = document.querySelector(".navbar--full-width");
+    let lastScroll=0;
     
-    if(currentScroll <= 0) {
-      navbar.classList.remove("scroll-up");
-    }
-  
-    if(currentScroll>lastScroll && !navbar.classList.contains("scroll-down")) {
-      navbar.classList.remove("scroll-up");
-      navbar.classList.add("scroll-down");
-    } else if(currentScroll<lastScroll && navbar.classList.contains("scroll-down")) {
-      navbar.classList.remove("scroll-down");
-      navbar.classList.add("scroll-up");
-    }
-  
-    lastScroll = currentScroll;
-  });
+    window.addEventListener("scroll", (e) => {
+      const currentScroll = window.scrollY;
+      
+      if(currentScroll <= 0) {
+        navbar.classList.remove("scroll-up");
+      }
+    
+      if(currentScroll>lastScroll && !navbar.classList.contains("scroll-down")) {
+        navbar.classList.remove("scroll-up");
+        navbar.classList.add("scroll-down");
+      } else if(currentScroll<lastScroll && navbar.classList.contains("scroll-down")) {
+        navbar.classList.remove("scroll-down");
+        navbar.classList.add("scroll-up");
+      }
+    
+      lastScroll = currentScroll;
+    });
+  }
 }
 
+new Navbar();
